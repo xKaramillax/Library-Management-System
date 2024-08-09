f1f197a45fee6a405e3e877b373ed8f00302ece3
xKaramillax <20220340@nctu.edu.eg>
2024-08-09 15:36:47 +0300

    Add files via upload

---
+++ Library-Management-System/src/main/java/com/mehmetpekdemir/librarymanagementsystem/LibrarymanagementsystemApplication.java
@@ -0,0 +1,61 @@
+package com.mehmetpekdemir.librarymanagementsystem;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+
+import com.mehmetpekdemir.librarymanagementsystem.entity.Author;
+import com.mehmetpekdemir.librarymanagementsystem.entity.Book;
+import com.mehmetpekdemir.librarymanagementsystem.entity.Category;
+import com.mehmetpekdemir.librarymanagementsystem.entity.Publisher;
+import com.mehmetpekdemir.librarymanagementsystem.service.BookService;
+
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+public class LibrarymanagementsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrarymanagementsystemApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner initialCreate(BookService bookService) {
+		return (args) -> {
+
+			Book book = new Book("000-0-00-000", "name", "serial name", "Test");
+			Author author = new Author("author name", "Test description");
+			Category category = new Category("category name");
+			Publisher publisher = new Publisher("publisher name");
+
+			book.addAuthors(author);
+			book.addCategories(category);
+			book.addPublishers(publisher);
+
+			bookService.createBook(book);
+
+			Book book1 = new Book("000-0-00-0000", "name 1", "serial name 1", "Test 1");
+			Author author1 = new Author("author name 1", "Test description 1");
+			Category category1 = new Category("category name 1");
+			Publisher publisher1 = new Publisher("publisher name 1");
+
+			book1.addAuthors(author1);
+			book1.addCategories(category1);
+			book1.addPublishers(publisher1);
+
+			bookService.createBook(book1);
+
+			Book book2 = new Book("000-00-00-0000", "name 2", "serial name 2", "Test 2");
+			Author author2 = new Author("author name 2", "Test description 2");
+			Category category2 = new Category("category name 2");
+			Publisher publisher2 = new Publisher("publisher name 2");
+
+			book2.addAuthors(author2);
+			book2.addCategories(category2);
+			book2.addPublishers(publisher2);
+
+			bookService.createBook(book2);
+
+		};
+	}
+}
